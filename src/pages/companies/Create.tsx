1de96d356448b759c472ce56a1db840a7cdf4d03
NayeraGad <naiera.gad30@gmail.com>
2024-10-10 20:24:46 +0300

    Create company

---
+++ src/pages/companies/Create.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Companies } from ".";
+import { Form, Input, Modal, Select } from "antd";
+import { useModalForm, useSelect } from "@refinedev/antd";
+import { useGo } from "@refinedev/core";
+import { CREATE_COMPANY_MUTATION } from "@/graphql/mutations";
+import { USERS_SELECT_QUERY } from "@/graphql/queries";
+import SelectOptionWithAvatar from "@/components/SelectOptionWithAvatar";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { UsersSelectQuery } from "@/graphql/types";
+
+export const Create = () => {
+  const go = useGo();
+
+  const goToListPage = () => {
+    go({
+      to: { resource: "companies", action: "list" },
+      options: { keepQuery: true },
+      type: "replace",
+    });
+  };
+
+  const { formProps, modalProps } = useModalForm({
+    redirect: false,
+    meta: {
+      gqlMutation: CREATE_COMPANY_MUTATION,
+    },
+    action: "create",
+    defaultVisible: true,
+    resource: "companies",
+    mutationMode: "pessimistic",
+    onMutationSuccess: goToListPage,
+  });
+
+  const { selectProps, query } = useSelect<GetFieldsFromList<UsersSelectQuery>>({
+    resource: "users",
+    meta: {
+      gqlQuery: USERS_SELECT_QUERY,
+    },
+    optionLabel: "name",
+  });
+
+  return (
+    <Companies>
+      <Modal
+        {...modalProps}
+        mask={true}
+        title="Create Company"
+        onCancel={goToListPage}
+        width={512}
+      >
+        <Form {...formProps} layout="vertical">
+          <Form.Item
+            label="Company name"
+            name="name"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="Please enter a company name" />
+          </Form.Item>
+
+          <Form.Item
+            label="Sales owner"
+            name="salesOwnerId"
+            rules={[{ required: true }]}
+          >
+            <Select
+              placeholder="Please enter sales owner user"
+              {...selectProps}
+              options={
+                query.data?.data.map((user) => ({
+                  value: user.id,
+                  label: (
+                    <SelectOptionWithAvatar
+                      name={user.name}
+                      avatarUrl={user.avatarUrl ?? undefined}
+                    />
+                  ),
+                })) ?? []
+              }
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </Companies>
+  );
+};
--- src/pages/companies/index.tsx
+++ src/pages/companies/index.tsx
@@ -16,21 +16,22 @@ import { Text } from "@/components/text";
 import { Company } from "@/graphql/schema.types";
 import { currencyNumber } from "@/Utilities";
 
-export const Companies = () => {
+export const Companies = ({ children }: React.PropsWithChildren) => {
   const go = useGo();
-  const { tableProps, filters } = useTable({
+  
+  const { tableProps, filters } = useTable<Company>({
     resource: "companies",
     meta: {
       gqlQuery: COMPANIES_LIST_QUERY,
     },
     onSearch: (value) => {
-        return [
-            {
-                field: 'name',
-                operator: 'contains',
-                value: value.name
-            }
-        ]
+      return [
+        {
+          field: "name",
+          operator: "contains",
+          value: value.name,
+        },
+      ];
     },
     pagination: {
       pageSize: 12,
@@ -53,83 +54,85 @@ export const Companies = () => {
       ],
     },
   });
-  console.log(tableProps);
-  
 
   return (
-    <List
-      breadcrumb={false}
-      headerButtons={() => (
-        <CreateButton
-          onClick={() => {
-            go({
-              to: {
-                resource: "companies",
-                action: "create",
-              },
-              options: {
-                keepQuery: true,
-              },
-              // There are three types: 'replace, 'push', 'reload'
-              type: "replace",
-            });
-          }}
-        />
-      )}
-    >
-      <Table
-        {...tableProps}
-        pagination={{
-          ...tableProps.pagination,
-          showTotal: () => (
-            <Text>{tableProps?.pagination?.total} companies in total</Text>
-          ),
-        }}
+    <div>
+      <List
+        breadcrumb={false}
+        headerButtons={() => (
+          <CreateButton
+            onClick={() => {
+              go({
+                to: {
+                  resource: "companies",
+                  action: "create",
+                },
+                options: {
+                  keepQuery: true,
+                },
+                // There are three types: 'replace, 'push', 'reload'
+                type: "replace",
+              });
+            }}
+          />
+        )}
       >
-        <Table.Column<Company>
-          dataIndex="name"
-          title="Company title"
-          defaultFilteredValue={getDefaultFilter("id", filters)}
-          filterIcon={<SearchOutlined />}
-          filterDropdown={(props) => (
-            <FilterDropdown {...props}>
-              <Input placeholder="Search Company" />
-            </FilterDropdown>
-          )}
-          render={(value, record) => (
-            <Space>
-              <CustomAvatar
-                shape="square"
-                name={record.name}
-                src={record.avatarUrl}
-              />
-              <Text style={{ whiteSpace: "nowrap" }}>{record.name}</Text>
-            </Space>
-          )}
-        />
+        <Table
+          {...tableProps}
+          pagination={{
+            ...tableProps.pagination,
+            showTotal: () => (
+              <Text>{tableProps?.pagination?.total} companies in total</Text>
+            ),
+          }}
+        >
+          <Table.Column<Company>
+            dataIndex="name"
+            title="Company title"
+            defaultFilteredValue={getDefaultFilter("id", filters)}
+            filterIcon={<SearchOutlined />}
+            filterDropdown={(props) => (
+              <FilterDropdown {...props}>
+                <Input placeholder="Search Company" />
+              </FilterDropdown>
+            )}
+            render={(value, record) => (
+              <Space>
+                <CustomAvatar
+                  shape="square"
+                  name={record.name}
+                  src={record.avatarUrl}
+                />
+                <Text style={{ whiteSpace: "nowrap" }}>{record.name}</Text>
+              </Space>
+            )}
+          />
 
-        <Table.Column<Company>
-          dataIndex="totalRevenue"
-          title="Open deals amount"
-          render={(value, company) => (
-            <Text>
-              {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
-            </Text>
-          )}
-        />
+          <Table.Column<Company>
+            dataIndex="totalRevenue"
+            title="Open deals amount"
+            render={(value, company) => (
+              <Text>
+                {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+              </Text>
+            )}
+          />
 
-        <Table.Column
-          dataIndex="id"
-          title="Actions"
-          fixed="right"
-          render={(value) => (
-            <Space>
-              <EditButton hideText size="small" recordItemId={value} />
-              <DeleteButton hideText size="small" recordItemId={value} />
-            </Space>
-          )}
-        />
-      </Table>
-    </List>
+          <Table.Column
+            dataIndex="id"
+            title="Actions"
+            fixed="right"
+            render={(value) => (
+              <Space>
+                <EditButton hideText size="small" recordItemId={value} />
+                <DeleteButton hideText size="small" recordItemId={value} />
+              </Space>
+            )}
+          />
+        </Table>
+      </List>
+
+      {children}
+    </div>
   );
 };
